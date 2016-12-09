3c0840c28f79725e69a6c34c040e1c9b9e977467
Chun Ly <astro.chun@gmail.com>
2016-12-09 16:53:57 -0700

    Fix __init__.py

--- __init__.py
+++ __init__.py
@@ -9,11 +9,3 @@ How to use the documentation
 
 import Zcalbase as Zcb
 """
-<<<<<<< HEAD
-=======
-
-from __future__ import absolute_import
-
-import sys
-import os
->>>>>>> 35034d88a92ab324c001a73f235d4da5b0dcd0b9
