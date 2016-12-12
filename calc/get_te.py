898be9b2a1072002e59c5df2a9e669571a0351c2
Chun Ly <astro.chun@gmail.com>
2016-12-11 21:49:04 -0700

    Document calc.get_te

--- calc/get_te.py
+++ calc/get_te.py
@@ -1,3 +1,14 @@
+"""
+get_te
+====
+
+Set of routines used by Zcalbase to compute electron temperatures
+
+Requirements:
+ PyNeb (http://www.iac.es/proyecto/PyNeb/)
+
+"""
+
 import numpy as np
 import pyneb as pn
 from matplotlib import pyplot as plt
@@ -20,14 +31,33 @@ def get_oiii(ratio0, silent=True):
 #'N2': {'atom': 'n_ii_atom_GMZ97-WFD96.dat', 'coll': 'n_ii_coll_LB94.dat'},
 
 def nicholls14():
+    '''
+    Function to compute electron temperature (T_e) of O++ from
+    [OIII]4959,5007/OIII4363. Values are based on Eq. (2) in
+    Nicholls et al. (2014), ApJ, 790, 75
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    10**iR : array or array-like
+      Flux ratio of [OIII] lines: I(4959+5007)/I(4363)
+
+    Te_OIII : array or array-like
+      Electron temperature of O++ in Kelvins
+    '''
+
+    # From page 4
     a = 13205.
     b = 0.92506
     c = 0.98062
   
-    iR = np.arange(1.0, 4.3, 0.01)
-    R = 1.0/10**(iR)
+    iR = np.arange(1.0, 4.3, 0.01) # log([OIII]4959,5007/[OIII]4363)
+    R = 1.0/10**(iR) # [OIII]4363/[OIII]4959,5007
     
-    Te_OIII = a*(-1*np.log10(R)-b)**(-c)
+    Te_OIII = a*(-1*np.log10(R)-b)**(-c) # in Kelvin
     return 10**iR, Te_OIII
 #enddef
     
@@ -45,6 +75,7 @@ def plot_R_Te():
     ax.set_xlabel(r'$\frac{I(\lambda4959) + I(\lambda5007)}{I(\lambda4363)}$')
     ax.set_ylabel(r'$T_e$ (K)')
 
+    # Get Nicholls calibration
     Nic14_R, Nic14_Te = nicholls14()
     ax.plot(Nic14_R, Nic14_Te, 'k-')
     
